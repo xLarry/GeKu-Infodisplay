81eaea5d183f5e8e76bd0394a732f757173d932b
Robin Böhm <robin.boehm@symetics.com>
2016-03-27 16:05:19 +0200

    refactor(sunrise): use es6 fat arrow and readable variable names

--- frontend-src/js/components/sunrise/sunrise.component.js
+++ frontend-src/js/components/sunrise/sunrise.component.js
@@ -1,9 +1,12 @@
 angular.module("GekuInfodisplay")
   .component('sunrise', {
     templateUrl : 'js/components/sunrise/sunrise.component.html',
-    controller  : function (SunriseService) {
-      SunriseService().then(function (data) {
-        this.sunData = data;
-      }.bind(this))
+    controller  : function ($interval, SunriseService) {
+      var updateSunriseData = () => SunriseService().then(data => this.sunData = data);
+      updateSunriseData();
+
+      // hourly update
+      var hourly = 1000*60*60;
+      $interval(updateSunriseData, hourly);
     }
   });
