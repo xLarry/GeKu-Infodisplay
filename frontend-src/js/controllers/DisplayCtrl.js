9fc84a2353a574d62a6b1f514aa73a90ce19ccc8
Robin Böhm <robin.boehm@symetics.com>
2016-03-27 23:27:02 +0200

    feat(display): cleanup

--- frontend-src/js/controllers/DisplayCtrl.js
+++ frontend-src/js/controllers/DisplayCtrl.js
@@ -1,14 +1,21 @@
 angular.module("GekuInfodisplay")
 
-  .controller("DisplayCtrl", function (CalendarService, MessageService, SaunaService, WeatherService, $interval, $location) {
-
-    var display = this;
+  .controller("DisplayCtrl", function (CalendarService, MessageService, SaunaService, WeatherService) {
 
     var skycons = new Skycons();
 
-    display.events = [];
-    display.sauna = {};
-    display.weather = {};
+    var onWeatherUpdate = (weather) => {
+      this.weather = weather;
+      skycons.add("weatherCurrent", this.weather.currently.icon);
+      skycons.add("weatherDay1", this.weather.daily.data[1].icon);
+      skycons.add("weatherDay2", this.weather.daily.data[2].icon);
+      skycons.add("weatherDay3", this.weather.daily.data[3].icon);
+      skycons.play();
+    };
+
+    /* Weather */
+
+    WeatherService.events.subscribe(onWeatherUpdate);
 
     /* Events */
 
@@ -18,26 +25,13 @@ angular.module("GekuInfodisplay")
 
     SaunaService.events.subscribe((sauna) => this.sauna = sauna);
 
-    /* Weather */
 
-    function onWeatherUpdate (weather) {
-      display.weather = weather;
-      skycons.add("weatherCurrent", display.weather.currently.icon);
-      skycons.add("weatherDay1", display.weather.daily.data[1].icon);
-      skycons.add("weatherDay2", display.weather.daily.data[2].icon);
-      skycons.add("weatherDay3", display.weather.daily.data[3].icon);
-      skycons.play();
+    this.getTemp = (offset) => {
+      if (!this.weather || !this.weather.daily) return false;
+      return (this.weather.daily.data[offset].temperatureMin + this.weather.daily.data[offset].temperatureMax) / 2;
     }
 
-    // Add RealTime Support
-    WeatherService.events.subscribe(onWeatherUpdate);
-
-    display.getTemp = function getTemp (offset) {
-      if (!display.weather.daily) return false;
-      return (display.weather.daily.data[offset].temperatureMin + display.weather.daily.data[offset].temperatureMax) / 2;
-    }
-
-    display.getWeekday = function getWeekday (offset) {
+    this.getWeekday = (offset) => {
       return moment().add(offset, 'days').format('dddd');
     }
   });
