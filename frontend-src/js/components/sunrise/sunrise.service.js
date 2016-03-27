e0bded11932b0bd551db52a30bcf8d7a866176c3
Robin Böhm <robin.boehm@symetics.com>
2016-03-27 19:02:17 +0200

    fix(sunrise): use forecast api service

--- frontend-src/js/components/sunrise/sunrise.service.js
+++ frontend-src/js/components/sunrise/sunrise.service.js
@@ -1,21 +1,12 @@
 angular.module('GekuInfodisplay')
-  .factory('SunriseService', function ($http) {
-    var today = new Date();
-
-    function getDateAsString (date) {
-      var yyyy = date.getFullYear().toString();
-      var mm = (date.getMonth() + 1).toString(); // getMonth() is zero-based
-      var dd = date.getDate().toString();
-      return yyyy + '-' + mm + '-' + dd
-    }
-
+  .factory('SunriseService', function (WeatherService) {
     return function () {
       //var lat = 51.459265, long = 7.013491;
-      return $http.get('http://api.sunrise-sunset.org/json?lat=51.459265&lng=7.013491&date=today')
-        .then(function (response) {
+      return WeatherService.get().$promise
+        .then(function (weather) {
           return {
-            sunrise : Date.parse(getDateAsString(today) + ' ' + response.data.results.sunrise + ' GMT'),
-            sunset  : Date.parse(getDateAsString(today) + ' ' + response.data.results.sunset + ' GMT')
+            sunrise : weather.daily.data[0].sunriseTime * 1000,
+            sunset  : weather.daily.data[0].sunsetTime * 1000
           }
         });
     }
