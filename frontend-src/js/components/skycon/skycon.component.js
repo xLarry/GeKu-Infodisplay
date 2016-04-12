8f17d3f7a7c9a4cce618adc71dec087f0d5e41fa
Robin Böhm <robin.boehm@symetics.com>
2016-04-12 20:06:10 +0200

    feat(skycon): add basic component for skycon

---
+++ frontend-src/js/components/skycon/skycon.component.js
@@ -0,0 +1,25 @@
+angular.module("GekuInfodisplay")
+  .component('skycon', {
+    template : '<canvas width="75" height="75"></canvas>',
+    bindings : {
+      data : '='
+    },
+    controller : class {
+
+      constructor ($element) {
+        this.skycons = new Skycons();
+        this.element = $element;
+      }
+
+      $postLink () {
+        this.skycons.add(this.element.find('canvas')[0], this.data.icon);
+        this.skycons.play();
+      }
+
+      $onChanges () {
+        this.skycons.set(this.element.find('canvas')[0], this.data.icon);
+      }
+
+    }
+  });
+
