1913c067ff76ae25048534f60f7ac6794061b2c0
christinawunderlich <christina.wunderlich@gmail.com>
2016-10-03 22:19:40 +0200

    fix time interval for clock

--- frontend-src/js/components/clock/clock.component.js
+++ frontend-src/js/components/clock/clock.component.js
@@ -2,6 +2,16 @@ angular.module("GekuInfodisplay")
   .component('clock', {
     templateUrl : 'js/components/clock/clock.component.html',
     controller  : function ($interval) {
-        this.currentTime = new Date()
+        
+        
+        var $ctrl = this;
+        
+        function setCurrentDate() {
+            $ctrl.currentTime = new Date()
+        }
+
+        // Update Time once a Minute
+        $interval(setCurrentDate, 1000);
+        setCurrentDate();
     }
   });
